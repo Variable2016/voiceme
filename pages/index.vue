de577b673727467586d7268f8a5e6bbefc2afb4d
Yusuf Adeyemo <Yusadolat@gmail.com>
2019-09-13 16:33:50 +0100

    remove navbar sticky :bug:

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="leading-normal tracking-normal text-white gradient font-body">
-    <nav id="header" class="fixed w-full z-30 top-0 text-white">
+    <nav id="header" class="w-full z-30 text-white">
       <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div class="pl-4 flex items-center">
           <a
