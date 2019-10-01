46050926b2fc6e2287c439aff9f66e2848af9dd1
Yusadolat <yusadolat@gmail.com>
2019-10-01 17:33:07 +0100

    Added device used :rocket:

--- components/requesttrial.vue
+++ components/requesttrial.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto px-8 lg:px-48">
-    <form class="w-full max-w-sm">
+    <form class="w-full max-w-6xl">
       <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/3">
           <label
@@ -22,7 +22,24 @@
           <label
             class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             for="inline-username"
-          >Password</label>
+          >Email</label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-username"
+            type="email"
+            placeholder="name@example.com"
+          />
+        </div>
+      </div>
+
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-username"
+          >Company</label>
         </div>
         <div class="md:w-2/3">
           <input
@@ -33,18 +50,47 @@
           />
         </div>
       </div>
+
       <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3"></div>
-        <label class="md:w-2/3 block text-gray-500 font-bold">
-          <input class="mr-2 leading-tight" type="checkbox" />
-          <span class="text-sm">Send me your newsletter!</span>
-        </label>
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-username"
+          >Your role</label>
+        </div>
+        <div class="md:w-2/3">
+          <input
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="inline-username"
+            type="text"
+            placeholder="What's your role in your company?"
+          />
+        </div>
       </div>
-      <div class="md:flex md:items-center">
+
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label
+            class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+            for="inline-username"
+          >Device used</label>
+        </div>
+        <div class="md:w-2/3">
+          <select
+            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          >
+            <option value="Walkie Talkie">Walkie-Takie</option>
+            <option value="Smartphones">Smartphones</option>
+            <option value="both">Both</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="md:flex md:items-center mb-6">
         <div class="md:w-1/3"></div>
         <div class="md:w-2/3">
           <button
-            class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            class="shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="button"
           >Sign Up</button>
         </div>
