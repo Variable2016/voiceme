fd2aec545de6f7c1fc21327ff1b46b4a567b1e6a
Yusadolat <yusadolat@gmail.com>
2019-10-02 10:49:16 +0100

    Added Hambuger menu to About us page :fire: :fire:

--- pages/about.vue
+++ pages/about.vue
@@ -10,7 +10,41 @@
             to="/"
           >VoiceMe.AI</nuxt-link>
         </div>
+        <div class="block sm:hidden">
+          <button
+            @click="toggle"
+            class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
+          >
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
 
+        <div
+          :class="open ? 'block': 'hidden'"
+          class="w-full flex-grow sm:flex sm:items-center sm:w-auto"
+        >
+          <div class="text-sm sm:flex-grow">
+            <nuxt-link
+              class="block py-2 px-4 text-white sm:inline-block font-bolder text-lg no-underline mt-4"
+              to="/"
+            >Home</nuxt-link>
+            <nuxt-link
+              class="block py-2 px-4 text-white sm:inline-block font-bolder text-lg no-underline mt-4"
+              to="/dashboard"
+            >Dashboard</nuxt-link>
+            <nuxt-link
+              class="block py-2 px-4 text-white sm:inline-block font-bolder text-lg no-underline mt-4"
+              to="/about"
+            >About us</nuxt-link>
+          </div>
+        </div>
         <div
           class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
           id="nav-content"
@@ -338,6 +372,16 @@ import Navbar from "~/components/Navbar.vue";
 export default {
   components: {
     Navbar
+  },
+  data() {
+    return {
+      open: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open;
+    }
   }
 };
 </script>
