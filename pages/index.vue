a25237ee87af87dd93fe9f2593776ebc1cfa71df
Yusadolat <yusadolat@gmail.com>
2019-10-02 16:57:26 +0100

    Mobile fix :bug:

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="leading-normal tracking-normal text-white gradient font-body">
     <Navbar />
     <div class="pt-9">
-      <div class="container px-1 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
         <!--Left Col-->
         <div
           class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
