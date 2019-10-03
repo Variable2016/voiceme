12711fd0a987e09faeba5b52225094908395ad44
Yusadolat <yusadolat@gmail.com>
2019-10-03 18:26:14 +0100

    Description :zap:

--- components/requesttrial.vue
+++ components/requesttrial.vue
@@ -88,9 +88,9 @@
             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
             v-model="selected"
           >
-            <option value="Walkie Talkie">Walkie-Takie</option>
-            <option value="Smartphones">Smartphones</option>
-            <option value="both">Both</option>
+            <option>Walkie-Takie</option>
+            <option>Smartphones</option>
+            <option>Both</option>
           </select>
         </div>
       </div>
